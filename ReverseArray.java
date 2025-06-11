d8f8b0d541caba49b9c74c6759e600b889880314
DhanushaDudekula <dhanusha5689@gmail.com>
2025-06-11 10:47:32 +0530

    Add files via upload

---
+++ ReverseArray.java
@@ -0,0 +1,12 @@
+package com.dhanu;
+
+public class ReverseArray {
+	public static void main(String[] args) {
+		int[] arr={2,3,5,6,9};
+		for(int i=arr.length-1;i>=0;i--) 
+			System.out.print(arr[i]+" ");
+		}
+		
+	}
+
+
